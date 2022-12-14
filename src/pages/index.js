950f733252d1abd2e2eacf3ff2b22cae44df3e7d
supersliser <43965667+supersliser@users.noreply.github.com>
2022-12-14 15:00:38 +0000

    gsdgsgsgsg

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,8 +2,12 @@ import * as React from "react"
 import { Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
 
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+import * as styles from "../components/index.module.css"
+
 const IndexPage = () => (
-<p>hi</p>
+<p>Hi</p>
 )
 
 /**
