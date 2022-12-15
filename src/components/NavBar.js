88d31068e0cfd1a88bc1abe831fb999229bcf9e1
supersliser <43965667+supersliser@users.noreply.github.com>
2022-12-15 12:03:52 +0000

    acascacacaca

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -29,7 +29,7 @@ const NavItemContainer = styled.li`
     border-right-color: black;
     border-right-thickness: 2px;
     height: 100%;
-    width: 10%;
+    width: auto;
     text-align: center;
     padding-top: 5px;
     padding-bottom: 5px;
