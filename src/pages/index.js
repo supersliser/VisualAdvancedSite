891a7f3261fdb31fa6565d0629b55edc735c7f8e
supersliser <43965667+supersliser@users.noreply.github.com>
2022-12-15 10:40:44 +0000

    fix1

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,6 @@ import { StaticImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
-import * as styles from "../components/index.module.css"
 
 import NavBar from "../componenets/NavBar"
 
