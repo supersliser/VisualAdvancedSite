b6a97d7c6f0c29263630855f0a34e6c5bf61f332
supersliser <43965667+supersliser@users.noreply.github.com>
2022-12-16 10:34:31 +0000

    ftvgbnhmko,pl

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,8 +9,8 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `Visual Advanced`,
+    description: `The official site of the Visual Advanced Site`,
     author: `@gatsbyjs`,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
   },
