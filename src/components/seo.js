cd20628984f50c692e121337e6a0bb3dac9334e7
supersliser <thomaslower9@gmail.com>
2023-01-04 16:35:44 +0000

    dbgdbcvdf

--- src/components/seo.js
+++ src/components/seo.js
@@ -1,10 +1,3 @@
-/**
- * SEO component that queries for data with
- * Gatsby's useStaticQuery React hook
- *
- * See: https://www.gatsbyjs.com/docs/how-to/querying-data/use-static-query/
- */
-
 import * as React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 
@@ -33,10 +26,6 @@ function Seo({ description, title, children }) {
       <meta property="og:title" content={title} />
       <meta property="og:description" content={metaDescription} />
       <meta property="og:type" content="website" />
-      <meta name="twitter:card" content="summary" />
-      <meta name="twitter:creator" content={site.siteMetadata?.author || ``} />
-      <meta name="twitter:title" content={title} />
-      <meta name="twitter:description" content={metaDescription} />
       {children}
     </>
   )
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,14 @@
 import * as React from "react"
 import Seo from "../components/seo"
 
-import NavBar from "../components/NavBar"
 import Main from "../components/Main"
 
 const IndexPage = () => (
     <Main>
-        Hi
+        <p>hello world</p>
     </Main>
 )
 
-/**
- * Head export to define metadata for the page
- *
- * See: https://www.gatsbyjs.com/docs/reference/built-in-components/gatsby-head/
- */
 export const Head = () => <Seo title="Home" />
 
 export default IndexPage
