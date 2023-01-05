2c101243af075f0b5e2bdf7bc60c805caf71d376
supersliser <43965667+supersliser@users.noreply.github.com>
2023-01-05 13:14:34 +0000

    sdmjksdfjksdnjksfdnjksfd

--- src/components/newsBox.js
+++ src/components/newsBox.js
@@ -3,6 +3,7 @@ import styled from 'styled-components'
 import { StaticImage } from "gatsby-plugin-image"
 import Text from "./Text"
 const NewsBoxContainer = styled.div`
+    position: absolute;
     border-style: Solid;
     border-width: Medium;
     border-color: Yellow;
@@ -19,21 +20,23 @@ const TitleItem = styled(Text)`
     padding-top: 5%;
     padding-left: 5%;
     padding-right: 5%;
+    position: relative;
 `
 
 const SubtitleItem = styled(Text)`
     color: Yellow;
-    font-size: 3em;
+    font-size: 1em;
     width: 40%;
     height: 75%;
     padding-top: 15%;
     padding-left: 5%;
     padding-right: 5%;
+    position: relative;
 `
 
 const NewsBox = ({Width, Height, Title, Subtitle}) => (
     <NewsBoxContainer width="{Width}%" height="{Height}%">
-        <StaticImage width="45%" Height="90%" MarginLeft="5%" MarginRight="10%" src="../images/Comps.JPG" alt={Title}/>
+        <StaticImage width="45%" Height="90%" MarginLeft="5%" MarginRight="10%" src="../images/Comps.JPG" alt={Title} position="relative"/>
         <TitleItem>{Title}</TitleItem>
         <SubtitleItem>{Subtitle}</SubtitleItem>
     </NewsBoxContainer>
