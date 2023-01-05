b88c441fffb5d3576c364867d9e9e64e785d03bd
supersliser <43965667+supersliser@users.noreply.github.com>
2023-01-05 13:00:44 +0000

    nsanjksnjkajsnjkanjksdanjsdanjksdassnjksdnjksanjkdsdnjksdanjksdanksadsdnjksnjkdasnjkda

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,10 +4,12 @@ import { StaticImage } from "gatsby-plugin-image"
 
 import Main from "../components/Main"
 import Text from "../components/Text"
+import NewsBox from "../components/newsBox"
 
 const IndexPage = () => (
     <Main>
         <Text>Hello World</Text>
+        <NewsBox width="40" height="20" image="../images/Comps.JPG" Title="This is a test" Subtitle="Im testing something and i dont know if its going to work tbh"/>
     </Main>
 )
 
