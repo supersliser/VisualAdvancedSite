32e919d79982d6986645dee6b63eaa218c08f4cb
supersliser <thomaslower9@gmail.com>
2023-04-21 22:18:28 +0100

    gd

--- src/components/newsBox.js
+++ src/components/newsBox.js
@@ -61,7 +61,7 @@ const NewsBox = ({ title, subtitle, img, date }) => (
             alt="image relating to the article"
         />
         <TitleItem>{title}</TitleItem>
-        <SubtitleItem style={{ paddingBottom: "10%" }}>{date}</SubtitleItem>
+        <SubtitleItem style={{ paddingBottom:"10%", PaddingTop:"10%" }}>{date}</SubtitleItem>
         <SubtitleItem>{subtitle}</SubtitleItem>
     </NewsBoxContainer>
 )
